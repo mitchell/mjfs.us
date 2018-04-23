90552c7db66d9db7a9c2aeffa2ad27c196711062
mitchelljfs <mitchelljfsimon@gmail.com>
2018-04-23 12:49:42 -0700

    Replaced CRA template with simple hello world to get started. Setup eslint and conformed current files to standard.

---
+++ src/Website.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Website.css';
+
+class Website extends Component {
+  render() {
+    return (
+      <div className="Website">
+        <div className="Hello-World">
+          <p>Hello, my dudes.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Website;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Website from './Website';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Website />,
+  document.getElementById('root')
+);
 registerServiceWorker();
